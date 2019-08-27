db2496e744ff1ac817ac3260f5d0cfae319f65bd
roabaot <a-ahmed987@outlook.com>
2019-08-27 19:18:20 +0300

    add core

---
+++ src/components/core/Drawer.vue
@@ -0,0 +1,183 @@
+<template>
+  <v-navigation-drawer
+    v-if="responsive"
+    id="app-drawer"
+    v-model="inputValue"
+    app
+    dark
+    floating
+    persistent
+    width="260"
+  >
+    <template v-slot:prepend>
+      <v-list-item>
+        <v-list-item-avatar
+          color="white"
+        >
+          <v-img
+            :src="logo"
+            height="34"
+            contain
+          />
+        </v-list-item-avatar>
+        <v-list-item-title class="title">
+          Vuetify MD
+        </v-list-item-title>
+      </v-list-item>
+    </template>
+      <v-divider/>
+      <v-list>
+        <v-list-item
+        >
+          <v-text-field
+            class="purple-input search-input"
+            label="Search..."
+            color="purple"
+          />
+        </v-list-item>
+        <v-list-item
+          v-for="(link, i) in links"
+          :key="i"
+          :to="link.to"
+          active-class="purple"
+          class="v-list-item"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title
+            v-text="link.text"
+          />
+        </v-list-item>
+        
+      </v-list>
+      <template v-slot:append>
+          <v-list-item
+            disabled
+            active-class="primary"
+            class="v-list-item v-list-item--buy"
+            to="/upgrade"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-package-up</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="font-weight-light">
+              Upgrade To PRO
+            </v-list-item-title>
+          </v-list-item >
+        </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+// Utilities
+import {
+  mapMutations,
+  mapState
+} from 'vuex'
+
+export default {
+  data: () => ({
+    logo: './img/vuetifylogo.png',
+    links: [
+      {
+        to: '/dashboard',
+        icon: 'mdi-view-dashboard',
+        text: 'Dashboard'
+      },
+      {
+        to: '/user-profile',
+        icon: 'mdi-account',
+        text: 'User Profile'
+      },
+      {
+        to: '/table-list',
+        icon: 'mdi-clipboard-outline',
+        text: 'Table List'
+      },
+      {
+        to: '/typography',
+        icon: 'mdi-format-font',
+        text: 'Typography'
+      },
+      {
+        to: '/icons',
+        icon: 'mdi-chart-bubble',
+        text: 'Icons'
+      },
+      {
+        to: '/maps',
+        icon: 'mdi-map-marker',
+        text: 'Maps'
+      },
+      {
+        to: '/notifications',
+        icon: 'mdi-bell',
+        text: 'Notifications'
+      }
+    ],
+    responsive: false
+  }),
+  computed: {
+    ...mapState('app', ['color']),
+    inputValue: {
+      get () {
+        return this.$store.state.app.drawer
+      },
+      set (val) {
+        this.setDrawer(val)
+      }
+    },
+    items () {
+      return this.$t('Layout.View.items')
+    },
+    sidebarOverlayGradiant () {
+      return `${this.$store.state.app.sidebarBackgroundColor}, ${this.$store.state.app.sidebarBackgroundColor}`
+    }
+  },
+  mounted () {
+    this.onResponsiveInverted()
+    window.addEventListener('resize', this.onResponsiveInverted)
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onResponsiveInverted)
+  },
+  methods: {
+    ...mapMutations('app', ['setDrawer', 'toggleDrawer']),
+    onResponsiveInverted () {
+      if (window.innerWidth < 991) {
+        this.responsive = true
+      } else {
+        this.responsive = false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app-drawer {
+    .v-list-item {
+      border-radius: 4px;
+      margin: 0;
+
+      &--buy {
+        margin-top: auto;
+        margin-bottom: 17px;
+        color: #ffffff80!important;
+      }
+
+    }
+
+    .v-list-item--active {
+      box-shadow: 0 0px 20px -10px rgb(231, 153, 255), 0 4px 20px 0px rgba(0, 0, 0, 0.12), 0 0px 5px -5px rgb(231, 153, 255) !important;
+    }
+
+    .search-input {
+      margin-bottom: 30px !important;
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+
+  }
+</style>
---
+++ src/components/core/header.vue
@@ -0,0 +1,196 @@
+<template>
+  <v-app-bar
+    v-if="responsive"
+    id="app-header"
+    dense
+    fixed
+  >
+    <v-img
+      :src="logo"
+      class="mr-4"
+      contain
+      height="auto"
+      width="48"
+      max-width="48"
+      @click="$router.push('/')"
+    />
+    <v-menu
+      v-for="(item, i) in menu"
+      :key="i"
+      open-on-hover
+      bottom
+      offset-y
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          :to="item.to"
+          active-class="text-primary"
+          class="btnH"
+          style="font-size: 14px;"
+          text
+          v-on="on"
+        >
+          {{ item.text }}
+        </v-btn>
+      </template>
+      <v-list v-if="item.items!=null">
+        <v-list-item
+          v-for="(link, i) in item.items"
+          :key="i"
+          :to="link.to"
+        >
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <v-spacer />
+    <div style="font-size: 14px;">
+      <span>Welcome visitor, you can </span>
+      <router-link to="/">login </router-link>
+      <span>or </span>
+      <router-link to="/">create an account</router-link>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+// Utilities
+import {
+  mapMutations,
+  mapState
+} from 'vuex'
+
+export default {
+  data: () => ({
+    logo: './img/vuetifylogo.png',
+    links: [
+      {
+        to: '/dashboard',
+        icon: 'mdi-view-dashboard',
+        text: 'Dashboard'
+      },
+      {
+        to: '/user-profile',
+        icon: 'mdi-account',
+        text: 'User Profile'
+      },
+      {
+        to: '/table-list',
+        icon: 'mdi-clipboard-outline',
+        text: 'Table List'
+      },
+      {
+        to: '/typography',
+        icon: 'mdi-format-font',
+        text: 'Typography'
+      },
+      {
+        to: '/icons',
+        icon: 'mdi-chart-bubble',
+        text: 'Icons'
+      },
+      {
+        to: '/maps',
+        icon: 'mdi-map-marker',
+        text: 'Maps'
+      },
+      {
+        to: '/notifications',
+        icon: 'mdi-bell',
+        text: 'Notifications'
+      }
+    ],
+    menu: [
+      {
+        text: 'Home',
+        to: '/'
+      },
+      {
+        text: 'Other',
+        to: null,
+        items: [
+          {
+            to: '/dashboard',
+            icon: 'mdi-view-dashboard',
+            text: 'Dashboard'
+          },
+          {
+            to: '/user-profile',
+            icon: 'mdi-account',
+            text: 'User Profile'
+          },
+          {
+            to: '/table-list',
+            icon: 'mdi-clipboard-outline',
+            text: 'Table List'
+          },
+          {
+            to: '/typography',
+            icon: 'mdi-format-font',
+            text: 'Typography'
+          },
+          {
+            to: '/icons',
+            icon: 'mdi-chart-bubble',
+            text: 'Icons'
+          },
+          {
+            to: '/maps',
+            icon: 'mdi-map-marker',
+            text: 'Maps'
+          },
+          {
+            to: '/notifications',
+            icon: 'mdi-bell',
+            text: 'Notifications'
+          }
+        ]
+      }
+    ],
+    responsive: false
+  }),
+  computed: {
+    ...mapState('app', ['color'])
+  },
+
+  created () {
+    if (window.innerWidth < 991) {
+      this.responsive = false
+    } else {
+      this.responsive = true
+    }
+  },
+  mounted () {
+    this.onResponsiveInverted()
+    window.addEventListener('resize', this.onResponsiveInverted)
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.onResponsiveInverted)
+  },
+
+  methods: {
+    ...mapMutations('app', ['toggleDrawer']),
+    onResponsiveInverted () {
+      if (window.innerWidth < 991) {
+        this.responsive = false
+      } else {
+        this.responsive = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app-header {
+    .btnH {
+      margin: auto;
+      &:hover {
+        background-color: #9900cc2e;
+      }
+      &:active {
+        background-color: #9900cc59;
+      }
+    }
+  }
+</style>
